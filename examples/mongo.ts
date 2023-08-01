a87aba0f56949050f1b9fa56eca4d3cab887f866
saravanansivanathan <saravanan.sivanathan@ketch.com>
2023-07-31 23:12:37 -0700

    fix: doc and example

---
+++ examples/mongo.ts
@@ -0,0 +1,32 @@
+import { Kind, RequestStatus, RequestStatusReason } from '../src'
+import { MongoClient } from 'mongodb'
+
+async function HandleRequest(req, conn) {
+  let uri = `mongodb+srv://${conn.username}:${conn.password}@${conn.host}/?retryWrites=true&w=majority`
+  const client = new MongoClient(uri)
+  try {
+    await client.connect()
+    const database = client.db('sample_restaurants')
+    const restaurants = database.collection('restaurants')
+    await restaurants.deleteOne({ email: req.request.subject.email })
+  } catch (error) {
+    throw error
+  } finally {
+    await client.close()
+  }
+  return {
+    apiVersion: req.apiVersion,
+    kind: Kind.DeleteResponse,
+    metadata: req.metadata,
+    response: {
+      status: RequestStatus.CompletedRequestStatus,
+      reason: RequestStatusReason.Executed,
+      expectedCompletionTimestamp: Date.now(),
+      identities: req.request.identities,
+      subject: req.request.subject,
+      context: req.request.context,
+    },
+  }
+}
+
+export { HandleRequest }
